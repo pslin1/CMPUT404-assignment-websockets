e45043727c24a50a51e4af9f045491bcc5908e84
Pierre Lin <pslin@ualberta.ca>
2019-03-19 00:12:11 -0400

    sockets.py should be done, freetests pass

--- sockets.py
+++ sockets.py
@@ -59,6 +59,34 @@ class World:
     def world(self):
         return self.space
 
+# https://github.com/abramhindle/WebSocketsExamples/blob/master/broadcaster.py
+# Credit: Abram Hindle (https://github.com/abramhindle)
+gevents = list()
+clients = list()
+
+# https://github.com/abramhindle/WebSocketsExamples/blob/master/broadcaster.py
+# Credit: Abram Hindle (https://github.com/abramhindle)
+def send_all(msg):
+    for client in clients:
+        client.put( msg )
+
+# https://github.com/abramhindle/WebSocketsExamples/blob/master/broadcaster.py
+# Credit: Abram Hindle (https://github.com/abramhindle)
+def send_all_json(obj):
+    send_all( json.dumps(obj) )
+
+# https://github.com/abramhindle/WebSocketsExamples/blob/master/broadcaster.py
+# Credit: Abram Hindle (https://github.com/abramhindle)
+class Client:
+    def __init__(self):
+        self.queue = queue.Queue()
+
+    def put(self, v):
+        self.queue.put_nowait(v)
+
+    def get(self):
+        return self.queue.get()
+
 myWorld = World()        
 
 def set_listener( entity, data ):
@@ -69,19 +97,60 @@ myWorld.add_set_listener( set_listener )
 @app.route('/')
 def hello():
     '''Return something coherent here.. perhaps redirect to /static/index.html '''
-    return None
+    # https://github.com/abramhindle/WebSocketsExamples/blob/master/broadcaster.py
+    # Credit: Abram Hindle (https://github.com/abramhindle)
+    return flask.redirect("/static/index.html")
 
 def read_ws(ws,client):
     '''A greenlet function that reads from the websocket and updates the world'''
     # XXX: TODO IMPLEMENT ME
-    return None
+    # https://github.com/abramhindle/WebSocketsExamples/blob/master/broadcaster.py
+    # Credit: Abram Hindle (https://github.com/abramhindle)
+    try:
+        while True:
+            msg = ws.receive()
+            print("WS RECV: %s" % msg)
+            #msg is {"X0": {"x": 0, "y": 0}}
+
+            #packet is {'X0': {'x': 0, 'y': 0}}
+
+            if (msg is not None):
+                packet = json.loads(msg)
+                #print(type(packet))
+                #Note that packet is a dict
+                send_all_json(packet)
+                # https://stackoverflow.com/questions/2733813/iterating-through-a-json-object
+                # Credit: tzot (https://stackoverflow.com/users/6899/tzot)
+                for entity, data in packet.items():
+                    myWorld.set(entity, data)
+                #TODO, update world
+            else:
+                break
+    except:
+        '''Done'''
 
 @sockets.route('/subscribe')
 def subscribe_socket(ws):
     '''Fufill the websocket URL of /subscribe, every update notify the
        websocket and read updates from the websocket '''
     # XXX: TODO IMPLEMENT ME
-    return None
+    # https://github.com/abramhindle/WebSocketsExamples/blob/master/broadcaster.py
+    # Credit: Abram Hindle (https://github.com/abramhindle)
+    client = Client()
+    clients.append(client)
+    g = gevent.spawn( read_ws, ws, client)
+    print("Subscribing")
+    try:
+        while True:
+            msg = client.get()
+            print("Got a message!")
+            ws.send(msg)
+    except Exception as e:
+        print("WS Error %s" % e)
+    finally:
+        clients.remove(client)
+        gevent.kill(g)
+
 
 
 # I give this to you, this is how you get the raw body/data portion of a post in flask
@@ -99,23 +168,38 @@ def flask_post_json():
 @app.route("/entity/<entity>", methods=['POST','PUT'])
 def update(entity):
     '''update the entities via this interface'''
-    return None
+    # https://github.com/pslin1/CMPUT404-assignment-ajax/blob/master/server.py
+    # Credit: pslin1 (https://github.com/pslin1)
+    data = flask_post_json()
+    for key, value in data.items():
+        myWorld.update(entity, key, value)
+    #How do we return the updated entity?**
+    #https://stackoverflow.com/questions/34057851/python-flask-typeerror-dict-object-is-not-callable/34057946
+    #Credit: davidism (https://stackoverflow.com/users/400617/davidism)
+    return jsonify(myWorld.get(entity))
 
 @app.route("/world", methods=['POST','GET'])    
 def world():
     '''you should probably return the world here'''
-    return None
+    # https://github.com/pslin1/CMPUT404-assignment-ajax/blob/master/server.py
+    # Credit: pslin1 (https://github.com/pslin1)
+    return jsonify(myWorld.world())
 
 @app.route("/entity/<entity>")    
 def get_entity(entity):
     '''This is the GET version of the entity interface, return a representation of the entity'''
-    return None
+    # https://github.com/pslin1/CMPUT404-assignment-ajax/blob/master/server.py
+    # Credit: pslin1 (https://github.com/pslin1)
+    return jsonify(myWorld.get(entity))
 
 
 @app.route("/clear", methods=['POST','GET'])
 def clear():
     '''Clear the world out!'''
-    return None
+    # https://github.com/pslin1/CMPUT404-assignment-ajax/blob/master/server.py
+    # Credit: pslin1 (https://github.com/pslin1)
+    myWorld.clear()
+    return jsonify(myWorld.world())
 
 
 
